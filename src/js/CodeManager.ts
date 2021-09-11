5e7b2cdb936e92e023e990f5f96523f972dc9b35
dames <damgem@zoho.com>
2021-09-12 00:44:39 +0200

    add documentation for Runtime and CodeManager

--- src/js/CodeManager.ts
+++ src/js/CodeManager.ts
@@ -1,26 +1,133 @@
 import LineStatement from "./LineStatement";
 
-export default class CodeManager {
-
+/**
+ * Code Manager that mainly keeps track of what the next statement is.
+ */
+export default class CodeManager
+{
+    /**
+     * Callback that gets triggered when the instruction pointer is changed.
+     * {@link advanceInstructionPointer} can trigger this even when a planned
+     * jump results the intruction pointer to point to the same line.
+     */
     public onInstructionPointerChange?: (value: number) => void;
-    public onFinish?: () => void;
 
-    public constructor(statements: LineStatement[], onInstructionPointerChange?: (value: number) => void, onFinish?: () => void) {
+    /**
+     * Callback that gets triggered when this program is halted.
+     * This can potentially be triggered by {@link advanceInstructionPointer}
+     * or directly by {@link halt}.
+     */
+    public onHalt?: () => void;
+
+    /**
+     * Creates a new CodeManager instance.
+     * @param statements 
+     * @param onInstructionPointerChange value for
+     * {@link onInstructionPointerChange} callback
+     * @param onHalt value for {@link onHalt} callback
+     */
+    public constructor(statements: LineStatement[],
+                       onInstructionPointerChange?: (value: number) => void,
+                       onHalt?: () => void)
+    {
         this.setStatements(statements);
         this.onInstructionPointerChange = onInstructionPointerChange;
-        this.onFinish = onFinish;
+        this.onHalt = onHalt;
     }
 
-    public setStatements(statements: LineStatement[]) {
+    /**
+     * Sets the program. Regenerates the internal properties {@link labelMap}
+     * and {@link isRunning} (but never triggers {@link onHalt}).
+     * @param statements Program that is executed in this environment
+     */
+    public setStatements(statements: LineStatement[])
+    {
         this.statements = statements;
+        this.isRunning = this.instructionPointer < this.statements.length;
+        this.generateLabelMap();
+    }
 
-        this.isFinished = this.instructionPointer < this.statements.length;
+    /**
+     * Advances the instruction pointer to the next statement.
+     * The next statement is the statement planned by {@link planJump} or if
+     * none is planned the statement in the next line.
+     * @returns 
+     */
+    public advanceInstructionPointer()
+    { 
+        if(this.nextInstructionPointer !== undefined)
+        {
+            this.instructionPointer = this.nextInstructionPointer;
+            this.nextInstructionPointer = undefined;
+        }
+        else
+        {
+            this.instructionPointer += 1;
+            if(this.instructionPointer >= this.statements.length)
+            {
+                this.halt(); // will trigger onHalt
+                return;      // so don't trigger onInstructionPointerChange
+            }
+        }
 
+        this.onInstructionPointerChange?.(this.instructionPointer);
+    }
+
+    /**
+     * Plans a jump of the address pointer. Multiple calls will overwrite each
+     * other with only the last call being planned. Jump will be executed and 
+     * cleared by the next {@link advanceInstructionPointer} call.
+     * @param address line number or label to jump to
+     */
+    public planJump(address: number | string)
+    {
+        if(typeof address == 'string')
+        {
+            address = this.resolveLabel(address);
+        }
+        else if(address < 0 || address >= this.statements.length)
+        {
+            throw new Error(`Jump to invalid line: ${address}`);
+        }
+
+        console.assert(this.nextInstructionPointer === undefined,
+                       'Instruction Pointer nextValue not cleared?');
+
+        this.nextInstructionPointer = address;
+    }
+    
+    /**
+     * Halts the program. Will trigger {@link onHalt}.
+     */
+    public halt()
+    {
+        this.isRunning = false;
+        this.onHalt?.();
+    }
+
+    private resolveLabel(label: string): number
+    {
+        let num = this.labelMap.get(label);
+
+        if(num === undefined)
+        {
+            throw new Error(`Label not found: ${label}`);
+        }
+
+        return num;
+    }
+
+    private generateLabelMap() {
         this.labelMap.clear();
-        for(let si = 0; si < this.statements.length; si++) {
+
+        for(let si = 0; si < this.statements.length; si++)
+        {
             let label = this.statements[si].label;
-            if(label !== undefined) {
-                if(this.labelMap.has(label)) {
+
+            if(label !== undefined)
+            {
+                if(this.labelMap.has(label))
+                {
                     throw new Error(`Duplicate labels ${label}`);
                 }
                 this.labelMap.set(label, si);
@@ -28,47 +135,14 @@ export default class CodeManager {
         }
     }
 
-    public advanceInstructionPointer() { 
-        if(this.nextInstructionPointer !== undefined) {
-            this.instructionPointer = this.nextInstructionPointer;
-            this.nextInstructionPointer = undefined;
-        } else {
-            this.instructionPointer += 1;
-            if(this.instructionPointer >= this.statements.length) {
-                this.finish();
-                return;
-            }
-        }
-        this.onInstructionPointerChange?.(this.instructionPointer);
-    }
-
-    public planJump(address: number | string) {
-        if(typeof address == 'string') {
-            address = this.resolveLabel(address);
-        } else if(address < 0 || address >= this.statements.length) {
-            throw new Error(`Jump to invalid line: ${address}`);
-        }
-        console.assert(this.nextInstructionPointer === undefined, 'Instruction Pointer nextValue not cleared?');
-        this.nextInstructionPointer = address;
-    }
-    
-    public finish() {
-        this.isFinished = true;
-        this.onFinish?.();
-    }
-
-    private resolveLabel(label: string): number {
-        let num = this.labelMap.get(label);
-        if(num === undefined) {
-            throw new Error(`Label not found: ${label}`);
-        }
-        return num;
-    }
+    private isRunning = true;
 
     private statements: LineStatement[] = [];
     private labelMap: Map<string, number> = new Map<string, number>();
 
     private instructionPointer: number = 0;
+    
+    // keep track of planned jumps
+    // if undefined no jump has been planned
     private nextInstructionPointer?: number = undefined;
-    private isFinished = false;
 }
--- src/js/Runtime.ts
+++ src/js/Runtime.ts
@@ -12,7 +12,16 @@ export class RuntimeCallbacks {
     onProgramEnd?: () => void
 }
 
+/**
+ * Runtime that contains memory, stacks a instruction pointer and more. 
+ */
 export default class Runtime {
+
+    /**
+     * Creates a new Runtime instance.
+     * @param statements the program that runs in this environment
+     * @param callbacks callbacks for on change events
+     */
     public constructor(statements: LineStatement[], callbacks: RuntimeCallbacks) {
         this.memory = new Memory(callbacks.onMemoryWrite);
         this.specialMemory = new Memory(callbacks.onSpecialMemoryWrite);
@@ -20,14 +29,31 @@ export default class Runtime {
         this.stack = new Stack(callbacks.onStackChange);
         this.callStack = new Stack(callbacks.onCallStackChange);
     
-        this.codeManager = new CodeManager([], callbacks.onInstructionPointerChange, callbacks.onProgramEnd);
+        this.codeManager = new CodeManager(statements, callbacks.onInstructionPointerChange, callbacks.onProgramEnd);
     }
 
+    /**
+     * Memory for default register.
+     */
     public memory: Memory;
+
+    /**
+     * Memory for special register like accumulators and such.
+     */
     public specialMemory: Memory;
 
+    /**
+     * Stack that the user can push to / pop from.
+     */
     public stack: Stack<number>;
+
+    /**
+     * Call stack to keep track where to jump back to after returning.
+     */
     public callStack: Stack<number>;
 
+    /**
+     * Code Manager that manages the instruction pointer among other things.
+     */
     public codeManager: CodeManager;
 }
